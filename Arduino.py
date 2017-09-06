b50588777866a94a1cbb616d186d7fac65c69189
Janin Koch <mail@janinkoch.de>
2017-09-06 16:40:49 +0200

    arduino 1 led

--- Arduino.py
+++ Arduino.py
@@ -1,3 +1,34 @@
-import serial
+import serial # you need to install the pySerial :pyserial.sourceforge.net
+import time
+# your Serial port should be different!
+arduino = serial.Serial('/dev/tty.usbmodem1411', 9600)
+
+def onOffFunction():
+    command = 'on'
+      #raw_input("Type something..: (on/ off / bye )");
+    if command =="on" and counter<=5:
+        print counter
+        print "The LED is on..."
+        time.sleep(1)
+        arduino.write('H')
+        onOffFunction()
+        counter+=1
+    elif command =="off":
+        print "The LED is off..."
+        time.sleep(1)
+        arduino.write('L')
+        onOffFunction()
+        arduino.close()
+    elif command =="bye":
+        print "See You!..."
+        time.sleep(1)
+        arduino.close()
+    else:
+        print "Sorry..type another thing..!"
+        onOffFunction()
+
+time.sleep(2) #waiting the initialization...
 
 
+
+onOffFunction()
